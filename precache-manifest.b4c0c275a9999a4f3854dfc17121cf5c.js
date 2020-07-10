53d35a495dec5c23e57094d3629c63ea9fa1cb77
k- <mahat.mejor@gmail.com>
2020-07-09 19:21:25 -0500

    Updates

---
+++ precache-manifest.b4c0c275a9999a4f3854dfc17121cf5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c06a1bac537b7b5eec5b1959495b9b6",
+    "url": "/delayed-youtube-captions/index.html"
+  },
+  {
+    "revision": "ddfc3646493fb7fb7358",
+    "url": "/delayed-youtube-captions/static/css/main.e03f8766.chunk.css"
+  },
+  {
+    "revision": "9e923fde3e05dcbdffe4",
+    "url": "/delayed-youtube-captions/static/js/2.bfeab77f.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/delayed-youtube-captions/static/js/2.bfeab77f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfc3646493fb7fb7358",
+    "url": "/delayed-youtube-captions/static/js/main.9455ad46.chunk.js"
+  },
+  {
+    "revision": "474319270c2c8799420f",
+    "url": "/delayed-youtube-captions/static/js/runtime-main.f11f2aa0.js"
+  }
+]);
